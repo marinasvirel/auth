d8f72a16e434a0770dfb7bbbfc4f4da254a4ec40
Марина Зинченко <marusenka_83@mail.ru>
2024-02-16 15:46:40 +0300

    login

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+// ⊗ppPmAuAt
+require_once "db.php";
+
+if (!empty($_POST['login']) && !empty($_POST['password'])) {
+  $login = $_POST['login'];
+  $password = $_POST['password'];
+
+  $query = "SELECT * FROM users WHERE `login`='$login' AND `password`='$password'";
+  $res = mysqli_query($link, $query);
+  $user = mysqli_fetch_assoc($res);
+
+  if (!empty($user)) {
+    echo "прошел авторизацию";
+  } else {
+    echo "неверный логин или пароль";
+  }
+}
+?>
+
+<form action="" method="POST">
+  <input name="login" placeholder="логин">
+  <input name="password" type="password" placeholder="пароль">
+  <input type="submit">
+</form>
