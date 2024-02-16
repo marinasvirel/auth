fae9fa01993af35a8c0011d3e9f0056e10dad6f5
Марина Зинченко <marusenka_83@mail.ru>
2024-02-16 15:41:14 +0300

    init

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost'; // имя хоста
+$user = 'root';      // имя пользователя
+$pass = '';          // пароль
+$name = 'mydb';      // имя базы данных
+
+$link = mysqli_connect($host, $user, $pass, $name);
