c2ba83c257fb92ef46cef47b1acf108cc457c689
Марина Зинченко <marusenka_83@mail.ru>
2024-03-06 02:42:46 +0300

    date

--- register.php
+++ register.php
@@ -4,8 +4,9 @@ require_once "db.php";
 if (!empty($_POST['login']) and !empty($_POST['password'])) {
   $login = $_POST['login'];
   $password = $_POST['password'];
+  $date = $_POST['date'];
 
-  $query = "INSERT INTO users SET login='$login', password='$password'";
+  $query = "INSERT INTO users SET login='$login', password='$password', date='$date'";
   mysqli_query($link, $query);
 }
 ?>
@@ -13,5 +14,10 @@ if (!empty($_POST['login']) and !empty($_POST['password'])) {
 <form action="" method="POST">
   <input name="login" placeholder="логин">
   <input name="password" type="password" placeholder="пароль">
+  <input type="date" name="date">
   <input type="submit">
 </form>
+
+<?php
+// var_dump($_POST['date']);
+?>
