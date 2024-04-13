b7822d3cc43863c91a0eda095f8a8f2e4a6d0997
Марина Зинченко <marusenka_83@mail.ru>
2024-04-13 12:29:40 +0300

    users

---
+++ users.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "db.php";
+
+$query = "SELECT * FROM users";
+$res = mysqli_query($link, $query);
+while ($users = mysqli_fetch_assoc($res)) {
+  echo "<a href='profile.php?id={$users['id']}'>{$users['login']}</a>";
+  echo "<br>";
+}
+
