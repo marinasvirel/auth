1a709551973974f558b4373dcabf0c0ff5e01877
Марина Зинченко <marusenka_83@mail.ru>
2024-04-27 09:07:42 +0300

    table

--- admin.php
+++ admin.php
@@ -7,3 +7,20 @@ require_once "db.php";
 if (!empty($_SESSION['auth']) and $_SESSION['status'] === 'admin') {
   echo "Контент для админов";
 }
+
+$query = "SELECT * FROM users";
+$res = mysqli_query($link, $query);
+?>
+
+<table border="2px">
+  <tr>
+    <th>логин</th>
+    <th>статус</th>
+  </tr>
+  <?php while ($users = mysqli_fetch_assoc($res)): ?>
+    <tr>
+      <td><?= $users['login'] ?></td>
+      <td><?= $users['status'] ?></td>
+    </tr>
+  <?php endwhile; ?>
+</table>
