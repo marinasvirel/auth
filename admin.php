fd450ed5e98dc8c09965f8a5f735b6c3cc6f9318
Марина Зинченко <marusenka_83@mail.ru>
2024-04-30 08:13:02 +0300

    del

--- admin.php
+++ admin.php
@@ -4,23 +4,32 @@ session_start();
 
 require_once "db.php";
 
-if (!empty($_SESSION['auth']) and $_SESSION['status'] === 'admin') {
-  echo "Контент для админов";
-}
-
 $query = "SELECT * FROM users";
 $res = mysqli_query($link, $query);
 ?>
 
-<table border="2px">
-  <tr>
-    <th>логин</th>
-    <th>статус</th>
-  </tr>
-  <?php while ($users = mysqli_fetch_assoc($res)): ?>
+<?php if (!empty($_SESSION['auth']) and $_SESSION['status'] === 'admin'): ?>
+
+  <table border="2px">
     <tr>
-      <td><?= $users['login'] ?></td>
-      <td><?= $users['status'] ?></td>
+      <th>логин</th>
+      <th>статус</th>
     </tr>
-  <?php endwhile; ?>
-</table>
+    <?php while ($users = mysqli_fetch_assoc($res)): ?>
+      <tr>
+        <td><?= $users['login'] ?></td>
+        <td><?= $users['status'] ?></td>
+        <td><a href="?del=<?= $users['id'] ?>">удалить</a></td>
+      </tr>
+    <?php endwhile; ?>
+  </table>
+
+<?php endif; ?>
+
+<?php
+if (isset($_GET['del'])) {
+  $id = $_GET['del'];
+  $query = "DELETE FROM users WHERE id = $id";
+  $res = mysqli_query($link, $query);
+}
+?>
